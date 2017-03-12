b7863e3fcbe5d405a6954e794ccd3c6303a2440f
dziem <dziemboh@gmail.com>
2017-03-12 10:29:27 +0700

    Add files via upload

--- TUBES/tubes/main_procedure.cpp
+++ TUBES/tubes/main_procedure.cpp
@@ -54,8 +54,7 @@ void aksimenu(int menu, int sub, list_instruktur &l_ins, list_siswa &l_sis, list
             getch();
         break;
         case 2:
-            addSiswa(l_sis);
-            getch();
+            addSiswa(l_rel, l_ins, l_sis);
         break;
         case 3:
             addRelasi(l_rel,l_ins,l_sis);
@@ -110,9 +109,7 @@ void aksimenu(int menu, int sub, list_instruktur &l_ins, list_siswa &l_sis, list
             getch();
         break;
         case 3:
-            clearscreen();
-            cout << "Print list relasi" << endl;
-            printRelasi(l_rel);
+            printRel(l_rel);
             getch();
         break;
         }
@@ -150,19 +147,91 @@ void addInstruktur(list_instruktur &L){
     cin >> p.nama;
     cout << "Masukkan nomor telepon instruktur : ";
     cin >> p.telp;
-    insertInstruktur(L,alokasiInstruktur(p));
-    cout << endl << "Berhasil insert ke list instruktur" << endl;
+    address_instruktur i = findInstruktur(L, p);
+    if(i != nil){
+        cout << endl << "Gagal insert ke list instruktur, data sudah ada" << endl;
+    }else{
+        insertInstruktur(L,alokasiInstruktur(p));
+        cout << endl << "Berhasil insert ke list instruktur" << endl;
+    }
 }
-void addSiswa(list_siswa &L){
+void addSiswa(list_relasi &LR, list_instruktur LI, list_siswa &LS){
     infotype_siswa p;
     clearscreen();
+    bool stop = false;
+    bool stopYN = false;
+    string q;
     cout << "Insert list siswa" << endl;
     cout << "Masukkan nama siswa : ";
     cin >> p.nama;
     cout << "Masukkan alamat siswa : ";
     cin >> p.alamat;
-    insertSiswa(L,alokasiSiswa(p));
-    cout << endl << "Berhasil insert ke list siswa" << endl;
+    address_siswa s = findSiswa(LS, p);
+    if(s != nil){
+        cout << endl << "Gagal insert ke list siswa, data sudah ada" << endl;
+    }else{
+        insertSiswa(LS,alokasiSiswa(p));
+        do{
+            clearscreen();
+            cout << "Menambahkan relasi antara instruktur dan siswa yang baru di insert" << endl;
+            infotype_instruktur i;
+            cout << "Masukkan nama instruktur dari siswa " << p.nama << " : ";
+            cin >> i.nama;
+            cout << "Masukkan nomor telepon instruktur dari siswa " << p.nama << " : ";
+            cin >> i.telp;
+            address_instruktur adr_i = findInstruktur(LI,i);
+            address_siswa adr_s = findSiswa(LS, p);
+            if(adr_i != nil && adr_s != nil){
+                infotype_relasi r;
+                r.instruktur = adr_i;
+                r.siswa = adr_s;
+                address_relasi rel = findRelasi(LR, r);
+                if(rel != nil){
+                    cout << endl << "Gagal menambahkan relasi, relasi sudah ada" << endl;
+                }else{
+                    int j = countSiswaFromInst(LR, i);
+                    if(j < 5){
+                        insertRelasi(LR, alokasiRelasi(r));
+                        stop = true;
+                        cout << endl << "Berhasil insert ke list siswa dan menambahkan relasi" << endl << endl;
+                        getch();
+                    }else{
+                        cout << endl << "Gagal menambahkan relasi, kuota siswa instruktur sudah penuh" << endl;
+                        do{
+                            cout << "Input lagi ? (y/n) : ";
+                            cin >> q;
+                            if(q == "y" || q == "Y"){
+                                stopYN = true;
+                            }else if(q == "n" || q == "N"){
+                                stopYN = true;
+                                stop = true;
+                                deleteSiswa(LS, p);
+                            }else{
+                                cout << "Input salah, harus y atau n" << endl;
+                                stopYN = false;
+                            }
+                        }while(stopYN != true);
+                    }
+                }
+            }else{
+                cout << endl << "Gagal menambahkan relasi, instruktur tidak ada" << endl;
+                do{
+                    cout << "Input lagi ? (y/n) : ";
+                    cin >> q;
+                    if(q == "y" || q == "Y"){
+                        stopYN = true;
+                    }else if(q == "n" || q == "N"){
+                        stopYN = true;
+                        stop = true;
+                        deleteSiswa(LS, p);
+                    }else{
+                        cout << "Input salah, harus y atau n" << endl;
+                        stopYN = false;
+                    }
+                }while(stopYN != true);
+            }
+        }while(stop != true);
+    }
 }
 void addRelasi(list_relasi &LR, list_instruktur LI, list_siswa LS){
     clearscreen();
@@ -183,8 +252,18 @@ void addRelasi(list_relasi &LR, list_instruktur LI, list_siswa LS){
         infotype_relasi r;
         r.instruktur = adr_i;
         r.siswa = adr_s;
-        insertRelasi(LR, alokasiRelasi(r));
-        cout << endl << "Berhasil menambahkan relasi" << endl;
+        address_relasi rel = findRelasi(LR, r);
+        if(rel != nil){
+            cout << endl << "Gagal menambahkan relasi, relasi sudah ada" << endl;
+        }else{
+            int j = countSiswaFromInst(LR, i);
+            if(j < 5){
+                insertRelasi(LR, alokasiRelasi(r));
+                cout << endl << "Berhasil menambahkan relasi" << endl;
+            }else{
+                cout << endl << "Gagal menambahkan relasi, kuota siswa instruktur sudah penuh" << endl;
+            }
+        }
     }else{
         cout << endl << "Gagal menambahkan relasi" << endl;
     }
@@ -317,5 +396,125 @@ void findRel(list_relasi LR, list_instruktur LI, list_siswa LS){
         }
     }else{
         cout << "Relasi antara instruktur " << i.nama << " dan siswa " << s.nama << " tidak ada di dalam list" << endl;
+    }
+}
+
+/* Print from list function */
+void printRel(list_relasi LR){
+    bool stop = false;
+    int i;
+    clearscreen();
+    cout << "Pilih apa yang akan ditampilkan dari list relasi" << endl;
+    cout << "1. Tampilkan semua relasi" << endl;
+    cout << "2. Tampilkan siswa berdasarkan instruktur tertentu" << endl;
+    cout << "3. Tampilkan instruktur berdasarkan siswa tertentu" << endl;
+    cout << "0. Back" << endl;
+    do{
+        cout << "Pilih menu : ";
+        cin >> i;
+        if(i >= 0 && i <= 3){
+            stop = true;
+        }else{
+            cout << "Input salah" << endl;
+        }
+    }while(stop != true);
+    switch(i){
+    case 1:
+        cout << "Print semua relasi" << endl;
+        clearscreen();
+        printRelasi(LR);
+    break;
+    case 2:
+        clearscreen();
+        printSiswaFromIns(LR);
+    break;
+    case 3:
+        clearscreen();
+        printInsFromSiswa(LR);
+    break;
+    }
+}
+void printSiswaFromIns(list_relasi LR){
+    infotype_instruktur ins;
+    cout << "Print siswa berdasarkan instruktur tertentu" << endl;
+    cout << "Masukkan nama instruktur : ";
+    cin >> ins.nama;
+    cout << "Masukkan nomor telepon instruktur : ";
+    cin >> ins.telp;
+    if(first(LR) != nil && last(LR) != nil){
+        address_relasi q = first(LR);
+        int j = 1;
+        while(q != nil){
+            if(infoNama(addressInstruktur(q)) == ins.nama && infoTelp(addressInstruktur(q)) == ins.telp){
+                cout << "Siswa " << j << "." << endl;
+                cout << "Nama siswa : " << infoNama(addressSiswa(q)) << endl;
+                cout << "Alamat siswa : " << infoAlamat(addressSiswa(q)) << endl;
+                j++;
+            }
+            q = next(q);
+        }
+        if(j == 1){
+            cout << "Instruktur " << ins.nama << " tidak memiliki siswa" << endl;
+        }
+    }else{
+        cout << "List relasi kosong" << endl;
+    }
+}
+void printInsFromSiswa(list_relasi LR){
+    infotype_siswa sis;
+    cout << "Print instruktur berdasarkan siswa tertentu" << endl;
+    cout << "Masukkan nama siswa : ";
+    cin >> sis.nama;
+    cout << "Masukkan alamat siswa : ";
+    cin >> sis.alamat;
+    if(first(LR) != nil && last(LR) != nil){
+        address_relasi q = first(LR);
+        int j = 1;
+        while(q != nil){
+            if(infoNama(addressSiswa(q)) == sis.nama && infoAlamat(addressSiswa(q)) == sis.alamat){
+                cout << "Instruktur " << j << "." << endl;
+                cout << "Nama instruktur : " << infoNama(addressInstruktur(q)) << endl;
+                cout << "Nomor telepon instruktur : " << infoTelp(addressInstruktur(q)) << endl;
+                j++;
+            }
+            q = next(q);
+        }
+        if(j == 1){
+            cout << "Siswa " << sis.nama << " tidak memiliki instruktur" << endl;
+        }
+    }else{
+        cout << "List relasi kosong" << endl;
+    }
+}
+
+/* Fungsi operasi list relasi */
+int countSiswaFromInst(list_relasi L, infotype_instruktur p){
+    if(first(L) != nil && last(L) != nil){
+        address_relasi q = first(L);
+        int i = 0;
+        while(q != nil){
+            if(infoNama(addressInstruktur(q)) == p.nama && infoTelp(addressInstruktur(q)) == p.telp){
+                i++;
+            }
+            q = next(q);
+        }
+        return i;
+    }else{
+        return 0;
+    }
+}
+int countInstFromSiswa(list_relasi L, infotype_siswa p){
+    if(first(L) != nil && last(L) != nil){
+        address_relasi q = first(L);
+        int i = 0;
+        while(q != nil){
+            if(infoNama(addressSiswa(q)) == p.nama && infoAlamat(addressSiswa(q)) == p.alamat){
+                i++;
+            }
+            q = next(q);
+        }
+        return i;
+    }else{
+        return 0;
     }
 }
--- TUBES/tubes/main_procedure.h
+++ TUBES/tubes/main_procedure.h
@@ -13,7 +13,7 @@ int submenu();
 void aksimenu(int menu, int sub, list_instruktur &l_ins, list_siswa &l_sis, list_relasi &l_rel);
 void tampilmenu();
 void addInstruktur(list_instruktur &L);
-void addSiswa(list_siswa &L);
+void addSiswa(list_relasi &LR, list_instruktur LI, list_siswa &LS);
 void addRelasi(list_relasi &LR, list_instruktur LI, list_siswa LS);
 void delInstruktur(list_instruktur &L, list_relasi &LR);
 void delSiswa(list_siswa &L, list_relasi &LR);
@@ -21,5 +21,9 @@ void delRelasi(list_relasi &LR, list_instruktur LI, list_siswa LS);
 void findIns(list_instruktur L);
 void findSis(list_siswa L);
 void findRel(list_relasi LR, list_instruktur LI, list_siswa LS);
-
+void printRel(list_relasi LR);
+void printSiswaFromIns(list_relasi LR);
+void printInsFromSiswa(list_relasi LR);
+int countSiswaFromInst(list_relasi L, infotype_instruktur p);
+int countInstFromSiswa(list_relasi L, infotype_siswa p);
 #endif // MAIN_PROCEDURE_H_INCLUDED
